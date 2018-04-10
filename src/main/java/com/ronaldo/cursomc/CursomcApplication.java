5fc719253329be1bd0582ab547f3a376e93da664
Ronaldo C Leite <rleite1975@gmail.com>
2018-04-10 07:58:29 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/ronaldo/cursomc/CursomcApplication.java
+++ src/main/java/com/ronaldo/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ronaldo.cursomc.domain.Categoria;
+import com.ronaldo.cursomc.domain.Produto;
 import com.ronaldo.cursomc.repositories.CategoriaRepository;
+import com.ronaldo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
-	
+public class CursomcApplication implements CommandLineRunner {
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,14 +28,23 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		Categoria cat1  =  new Categoria(null, "Informática");
-		Categoria cat2  =  new Categoria(null, "Escritório");
-		
-		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 }
---
+++ src/main/java/com/ronaldo/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.ronaldo.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldo.cursomc.domain.Produto;
+
+@Repository
+public  interface ProdutoRepository extends JpaRepository<Produto, Serializable>{
+
+}
