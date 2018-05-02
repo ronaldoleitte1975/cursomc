a5ddbad2cc424618b8c3d037e66137e11f72b54c
Ronaldo C Leite <rleite1975@gmail.com>
2018-05-02 15:11:03 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/ronaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronaldo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ronaldo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,13 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produto(s).");
 		}
 
+	}
+
+	public List<Categoria> findAll() {
+	
+		return repo.findAll();
+		
+		
 	}
 
 }
