bde5457e5104f1ae5291bf16bcb313b74109ad82
Ronaldo C Leite <rleite1975@gmail.com>
2018-05-10 15:31:26 -0300

    Busca de pedidos por nome e categorias - PARTE 1 e 2

--- src/main/java/com/ronaldo/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/ronaldo/cursomc/repositories/CategoriaRepository.java
@@ -1,13 +1,12 @@
 package com.ronaldo.cursomc.repositories;
 
-import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ronaldo.cursomc.domain.Categoria;
 
 @Repository
-public  interface CategoriaRepository extends JpaRepository<Categoria, Serializable>{
+public  interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
 
 }
