f2eaf12b0c44371efba060748a273ad86fe86c15
Ronaldo C Leite <rleite1975@gmail.com>
2018-05-14 08:00:52 -0300

    Inserindo Pedido

---
+++ src/main/java/com/ronaldo/cursomc/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package com.ronaldo.cursomc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ronaldo.cursomc.domain.PagamentoComBoleto;
+import com.ronaldo.cursomc.domain.PagamentoComCartao;
+
+@Configuration
+public class JacksonConfig {
+	// https://stackoverflow.com/questions/41452598/overcome-can-not-construct-instance-ofinterfaceclass-
+	@Bean
+	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			public void configure(ObjectMapper objectMapper) {
+				objectMapper.registerSubtypes(PagamentoComCartao.class);
+				objectMapper.registerSubtypes(PagamentoComBoleto.class);
+				super.configure(objectMapper);
+			};
+		};
+		return builder;
+	}
+}
--- src/main/java/com/ronaldo/cursomc/services/PedidoService.java
+++ src/main/java/com/ronaldo/cursomc/services/PedidoService.java
@@ -1,13 +1,23 @@
 package com.ronaldo.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ronaldo.cursomc.domain.ItemPedido;
+import com.ronaldo.cursomc.domain.PagamentoComBoleto;
 import com.ronaldo.cursomc.domain.Pedido;
+import com.ronaldo.cursomc.domain.enums.EstadoPagamento;
+import com.ronaldo.cursomc.repositories.ItemPedidoRepository;
+import com.ronaldo.cursomc.repositories.PagamentoRepository;
 import com.ronaldo.cursomc.repositories.PedidoRepository;
 import com.ronaldo.cursomc.services.exceptions.ObjectNotFoundException;
+import com.ronaldo.cursomc.services.BoletoService;
 
 @Service
 public class PedidoService {
@@ -15,10 +25,54 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository ipRepository;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(@Valid Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		ipRepository.saveAll(obj.getItens());
+
+		return obj;
 	}
 
 }
